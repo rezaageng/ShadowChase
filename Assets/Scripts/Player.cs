3f0230adea941ae2d8b9ca767d595bff429215ce
Reza Ageng <rezaageng5@gmail.com>
2022-02-24 16:01:03 +0700

    add movement x

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+  [SerializeField]
+  private float moveForce = 10f;
+  [SerializeField]
+  private float jumpForce = 11f;
+  private float movementX;
+  private Rigidbody2D rb;
+  private SpriteRenderer sr;
+  private Animator anim;
+  private string WALK_ANIM = "Walk";
+
+  private void awake()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    sr = GetComponent<SpriteRenderer>();
+    anim = GetComponent<Animator>();
+  }
+
   // Start is called before the first frame update
   void Start()
   {
@@ -13,6 +30,12 @@ public class Player : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    PlayerMoveKeyboard();
+  }
 
+  private void PlayerMoveKeyboard()
+  {
+    movementX = Input.GetAxis("Horizontal");
+    transform.position += new Vector3(movementX, 0, 0) * moveForce * Time.deltaTime;
   }
 }
