a6d8a11469266d96e5541bee159634cbd248cac0
Reza Ageng <rezaageng5@gmail.com>
2022-02-26 08:05:41 +0700

    shadow script

---
+++ Assets/Scripts/Shadow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour
+{
+  [HideInInspector]
+  public float speed;
+  private Rigidbody2D rb;
+  private void Awake()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    speed = 7f;
+  }
+  // Update is called once per frame
+  private void FixedUpdate()
+  {
+    rb.velocity = new Vector2(speed, rb.velocity.y);
+  }
+}
