573ed1a7f633b8f61279e4bfeb9474110c9a06cb
Reza Ageng <rezaageng5@gmail.com>
2022-03-02 14:01:31 +0700

    add collector

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour
+{
+  private void OnTriggerEnter2D(Collider2D collider)
+  {
+    if (collider.CompareTag("Enemy") || collider.CompareTag("Player"))
+    {
+      Destroy(collider.gameObject);
+    }
+  }
+
+}
+
