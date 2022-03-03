686d804dffda0fb5294e30bbc23978f08c792d8e
Reza Ageng <rezaageng5@gmail.com>
2022-03-03 11:35:16 +0700

    game manager

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenuController : MonoBehaviour
 {
   public void PlayGame()
   {
-    SceneManager.LoadScene("Gameplay");
+    int clickedObj = int.Parse(EventSystem.current.currentSelectedGameObject.name);
+    int[] a = new int[10];
+    a[clickedObj] = 10;
+    // SceneManager.LoadScene("Gameplay");
   }
 }
