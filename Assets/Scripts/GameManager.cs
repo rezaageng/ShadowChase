f8c19eb9b8737413d087e0c174d3078c931c6288
Reza Ageng <rezaageng5@gmail.com>
2022-03-06 15:51:10 +0700

    add player selector

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -26,5 +27,24 @@ public class GameManager : MonoBehaviour
     {
       Destroy(gameObject);
     }
+  }
+
+  private void OnEnable()
+  {
+    SceneManager.sceneLoaded += OnLevelFinishedLoading;
+  }
+
+  private void OnDisable()
+  {
+    SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+  }
+
+  void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+  {
+    if (scene.name == "Gameplay")
+    {
+      Instantiate(players[_charIndex]);
+    }
+
   }
 }
